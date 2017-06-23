f92f799cf9d1a8d9646214a18b90a39df69e14f4
Andrew Chumich <andrewchumich@gmail.com>
2017-06-23 12:37:40 -0600

    change routes on login

--- reducers/user/user.saga.js
+++ reducers/user/user.saga.js
@@ -2,11 +2,20 @@ import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
 import { UserService } from './user.service';
 import type { UserAction } from './user.reducer';
 import { setUser, loginRequested, loginSucceeded, loginFailed, LOGIN_REQUESTED } from './user.reducer';
+import { actions } from 'react-native-navigation-redux-helpers';
+import { ROUTES } from '../../routes';
+const {
+  pushRoute,
+  popRoute,
+} = actions;
 
 function* login(action: UserAction) {
   try {
     const token = yield UserService.login(action.payload);
-    yield put(loginSucceeded())
+    yield put(loginSucceeded());
+    yield put(pushRoute({
+      key: ROUTES.map,
+    }, 'global'));
   } catch (e) {
     yield put(loginFailed(e));
   }
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+export const ROUTES = {
+  splashscreen:  'splashscreen',
+  login: 'login',
+  home:  'home',
+  blankPage: 'blankPage',
+  map: 'map',
+  manager: 'manager',
+};
